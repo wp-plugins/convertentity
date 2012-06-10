af1c7481720a506127499feaf46862a0b6df875e
plocha <plocha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-10 22:32:03 +0000

    first version
    git-svn-id: https://plugins.svn.wordpress.org/convertentity/trunk@555835 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ convertentity.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Plugin Name: Convertentity
+Plugin URI: http://wordpress.org/extend/plugins/convertentity/
+Description: WordPress outputs sometimes some character entity references wich are not allowed in XHTML. Convertentity replaces them with valid entities.
+Version: 1.0.0
+Author: plocha
+Author URI: http://profiles.wordpress.org/plocha
+License: GPL2
+*/
+
+/*
+/*
+Copyright 2012  Edgard Schmidt  (email : saiez@web.de)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class Convertentity
+{
+
+function __construct () {
+	add_action ( 'init', array ( $this, 'set_entity_filter' ) );
+}
+
+
+function set_entity_filter () {
+	ob_start ( array ( $this, 'filter_entities' ) );
+}
+
+
+function filter_entities ( $text ) {
+	$forbidden_entities  = get_html_translation_table (HTML_ENTITIES,
+		ENT_NOQUOTES | ENT_HTML5, get_bloginfo ( 'charset' ) );
+	foreach ( str_split ( '<>&' ) as $allowed) {
+		unset ($forbidden_entities [ $allowed ] );
+	}
+
+	return str_replace ( array_values ( $forbidden_entities ),
+		array_keys ( $forbidden_entities ), $text);
+}
+
+};
+
+new Convertentity ();
+
+?>
